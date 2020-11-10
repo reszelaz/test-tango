b8729990d856d1ec56178f5b13f685b38e1986f9
zreszela <zreszela@cells.es>
2020-11-10 22:17:16 +0100

    Refactor Device1

--- Device1.py
+++ Device1.py
@@ -2,6 +2,12 @@ import tango
 from tango.server import Device, attribute
 
 
+def read_double_scalar():
+    dev = tango.DeviceProxy("sys/tg_test/1")
+    dev.subscribe_event("double_scalar", tango.EventType.ATTR_CONF_EVENT, cb)
+    return dev.read_attribute("double_scalar").value
+
+
 def cb(*args):
     pass
 
@@ -11,8 +17,4 @@ class Device1(Device):
     attr1 = attribute()
 
     def read_attr1(self):
-        dev = tango.DeviceProxy("sys/tg_test/1")
-        dev.subscribe_event("double_scalar",
-                            tango.EventType.ATTR_CONF_EVENT,
-                            cb)
-        return dev.read_attribute("double_scalar").value
+        return read_double_scalar()
