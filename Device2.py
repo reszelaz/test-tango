365675e2a9f3c6c2df54ab43e265adf0206c407d
zreszela <zreszela@cells.es>
2020-11-10 22:19:22 +0100

    Add cmd2 to Device2

--- Device2.py
+++ Device2.py
@@ -1,7 +1,8 @@
 import time
+import threading
 
 import tango
-from tango.server import Device, attribute
+from tango.server import Device, attribute, command
 
 
 def create_device_proxy():
@@ -13,5 +14,21 @@ class Device2(Device):
 
     attr2 = attribute()
 
+    def init_device(self):
+        self.set_state(tango.DevState.ON)
+
     def read_attr2(self):
         return create_device_proxy()
+
+    def is_cmd2_allowed(self):
+        return self.get_state() == tango.DevState.ON
+
+    @command()
+    def cmd2(self):
+        self.set_state(tango.DevState.MOVING)
+
+        def job(device):
+            create_device_proxy()
+            device.set_state(tango.DevState.ON)
+
+        threading.Thread(target=job, args=(self, )).start()
