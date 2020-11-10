f8b0acbae93245b34bf7aae5ad1db335f8558843
zreszela <zreszela@cells.es>
2020-11-10 22:19:06 +0100

    Refactor Device2

--- Device2.py
+++ Device2.py
@@ -4,10 +4,14 @@ import tango
 from tango.server import Device, attribute
 
 
+def create_device_proxy():
+    tango.DeviceProxy("sys/tg_test/1")
+    return time.time()
+
+
 class Device2(Device):
 
     attr2 = attribute()
 
     def read_attr2(self):
-        tango.DeviceProxy("sys/tg_test/1")
-        return time.time()
+        return create_device_proxy()
